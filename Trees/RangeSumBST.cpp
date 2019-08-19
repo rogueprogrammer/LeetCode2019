953fe871f18eb5eebc830db972f9042cb07a9a40
rogueprogrammer <dkhanna@uwaterloo.ca>
2019-08-19 13:18:35 -0700

    Update RangeSumBST.cpp

--- Trees/RangeSumBST.cpp
+++ Trees/RangeSumBST.cpp
@@ -9,7 +9,11 @@
  */
  
  //https://leetcode.com/problems/range-sum-of-bst
- 
+
+/*ITERATIVE SOLUTION:
+Runtime: 160 ms, faster than 17.56% of C++ online submissions for Range Sum of BST.
+Memory Usage: 41.2 MB, less than 90.91% of C++ online submissions for Range Sum of BST.
+*/
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int L, int R) {
@@ -34,3 +38,27 @@ public:
         return sum;
     }
 };
+
+/* 
+RECURSIVE SOLUTION:
+Runtime: 144 ms, faster than 84.09% of C++ online submissions for Range Sum of BST.
+Memory Usage: 41.1 MB, less than 100.00% of C++ online submissions for Range Sum of BST.
+*/
+class Solution {
+public:
+    int sumUtil(TreeNode* root, int L, int R){
+        if(root == NULL) return 0;
+        if(root->val >= L && root->val <= R){
+            return root->val + sumUtil(root->left, L, R) + sumUtil(root->right, L, R);
+        } else{
+            return sumUtil(root->left, L, R) + sumUtil(root->right, L, R);
+        }
+    }
+    
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        int sum = 0;
+        if(root == NULL) return sum;
+        return sumUtil(root, L, R);   
+    }
+};
+
