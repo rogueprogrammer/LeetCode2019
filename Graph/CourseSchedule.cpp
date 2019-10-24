f7e23b97af975a369574a4b288f8cdfeeb42d397
rogueprogrammer <dkhanna@uwaterloo.ca>
2019-10-23 21:23:15 -0700

    Create CourseSchedule.cpp

---
+++ Graph/CourseSchedule.cpp
@@ -0,0 +1,53 @@
+#include "header.h"
+
+//https://leetcode.com/problems/course-schedule/
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+	if (prerequisites.size() == 0) return true;
+	//calculate indegree of each course.
+	//indegree is the number of nodes pointing to a node
+	//indegree can be seen as the number of prereqs for a course
+	vector<int> indegrees(numCourses, 0);
+	for (int i = 0; i < prerequisites.size(); ++i) {
+		int course = prerequisites[i][0];
+		indegrees[course]++;
+	}
+
+	//build the graph. course -> {list of courses I can take after taking current course}
+	map<int, vector<int>> adj;
+	for (int i = 0; i < prerequisites.size(); ++i) {
+		adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+	}
+	int count = 0; //number of nodes that has indegree = 0
+  
+	queue<int> q;
+	//add all courses that have indegree == 0
+  for (int i = 0; i < numCourses; i++) {
+		if (indegrees[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	while (!q.empty()) {
+		int top = q.front();
+		if (indegrees[top] == 0) {
+			++count;
+		}
+		vector<int> nextCourses = adj[top];
+		for (int i = 0; i < nextCourses.size(); ++i) {
+			indegrees[nextCourses[i]]--;
+			if (indegrees[nextCourses[i]] == 0) q.push(nextCourses[i]);
+		}
+		q.pop();
+	}
+	//return true if count == numCourses
+	return count == numCourses;
+}
+
+void main() {
+	int numCourses = 2;
+	vector<vector<int>> prerequisites = {
+		{0, 1}
+		{1, 0}
+	};
+	cout << canFinish(numCourses, prerequisites) << endl;
+}
