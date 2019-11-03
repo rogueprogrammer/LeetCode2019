b19bef997373f19685b039b16a9804710d618960
rogueprogrammer <dkhanna@uwaterloo.ca>
2019-11-03 16:05:54 -0500

    Update MostStonesRemovedWithSameRowOrCol.cpp

--- Graph/MostStonesRemovedWithSameRowOrCol.cpp
+++ Graph/MostStonesRemovedWithSameRowOrCol.cpp
@@ -1,23 +1,21 @@
-
 //https://leetcode.com/problems/most-stones-removed-with-same-row-or-column
-//passed 54/68 test cases, gets a TLE on test case with extremely large number of input values.
+//passed 62/68 test cases, gets a TLE on test case with extremely large number of input values.
 // no need for union find, just a standard BFS or DFS to find the number of connected components in the graph
-int removeStones(vector<vector<int>>& stones) {
-        //number of moves = number of stones - number of connected components
+//Compute: number of moves = number of stones - number of connected components
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
               
         //Step 1 - Build graph
         //map< pair<int, int>, vector< <pair<int, int>>>> adj;
-        map<vector<int>, vector<vector<int>>> adj;
+        map<int, vector<vector<int>> > rows;
+        map<int, vector<vector<int> >> cols;
         for(int i = 0; i < stones.size(); ++i){
-            vector<int> curStone = stones[i];
-            //find all other stones that share same row or col
-            for(int j = 0; j < stones.size(); ++j){
-                if(i == j) continue;
-                vector<int> otherStone = stones[j];
-                if(otherStone[0] == curStone[0] || otherStone[1] == curStone[1]){
-                    adj[curStone].push_back(otherStone);
-                }
-            }
+            vector<int> stone = stones[i];
+            int row = stone[0];
+            int col = stone[1];
+            rows[row].push_back(stone);
+            cols[col].push_back(stone);
         }
         
         //Step 2 - Compute number of components using BFS
@@ -39,7 +37,19 @@ int removeStones(vector<vector<int>>& stones) {
             while(!q.empty()){
                 vector<int> curStone = q.front();
                 visited.insert(curStone);
-                vector<vector<int>> neighbors = adj[curStone];
+                //vector<vector<int>> neighbors = adj[curStone];
+                vector<vector<int>> neighbors;
+                int row = curStone[0];
+                int col = curStone[1];
+                vector<vector<int>> allStonesInSameRow = rows[row];
+                vector<vector<int>> allStonesInSameCol = cols[col];
+                for(vector<int> r : allStonesInSameRow){
+                    neighbors.push_back(r);
+                }
+                for(vector<int> c : allStonesInSameCol){
+                    neighbors.push_back(c);
+                }
+                
                 for(vector<int> n : neighbors){
                     auto it = visited.find(n);
                     if(it == visited.end()){
@@ -50,5 +60,5 @@ int removeStones(vector<vector<int>>& stones) {
             }
         }
         return stones.size() - numComponents;
-        
     }
+};
