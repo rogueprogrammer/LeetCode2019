112a132d784e9101ff522a87cc72d3a60364215d
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-02-10 16:46:04 -0800

    Update TimeBasedKeyValueStore.cpp

--- Design/TimeBasedKeyValueStore.cpp
+++ Design/TimeBasedKeyValueStore.cpp
@@ -5,6 +5,30 @@ public:
 	TimeMap() {
 
 	}
+	map<string, map<int, string>> m;
+	void set(string key, string value, int timestamp) {
+		m[key][timestamp] = value;
+	}
+
+	string get(string key, int timestamp) {
+		auto end = m[key].upper_bound(timestamp);
+		string res = "";
+		if (end == m[key].begin()) {
+			return "";
+		}
+		return prev(end)->second;
+	}
+};
+
+// another possible solution:
+
+
+class TimeMap {
+public:
+	/** Initialize your data structure here. */
+	TimeMap() {
+
+	}
 	map<pair<string, int>, string> m;
 
 	void set(string key, string value, int timestamp) {
@@ -12,23 +36,11 @@ public:
 	}
 
 	string get(string key, int timestamp) {
-		auto start = m.lower_bound(pair<string, int>(key, 0));
 		auto end = m.upper_bound(pair<string, int>(key, timestamp));
 		string res = "";
-		int ts = 0;
-		for (auto it = start; it != end; ++it) {
-			if (it->first.second >= ts) {
-				ts = it->first.second;
-				res = it->second;
-			}
+		if (end == m.begin()) {
+			return res;
 		}
-		return res;
+		return prev(end)->second;
 	}
 };
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */
