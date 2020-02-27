4f7f8bb042832f250be9c7b24a52d2a78f583313
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-02-26 21:03:40 -0800

    Update BuildTreePreorderInorder.cpp

--- Trees/BuildTreePreorderInorder.cpp
+++ Trees/BuildTreePreorderInorder.cpp
@@ -12,17 +12,17 @@ public:
     
      TreeNode* buildSubtree(vector<int>& preorder, vector<int>& inorder,
      int preorderIndex, int inorderS, int inorderE) {
-     if (preorderIndex >= preorder.size() || inorderS > inorderE ){
-         return NULL;
-     }
-     int root = preorder[preorderIndex];
-     int i = 0;
-     for (i = 0; i < inorder.size(); ++i) { if (inorder[i] == root) { break; } }
-     TreeNode* res = new TreeNode(root);
-     res->left = buildSubtree(preorder, inorder, preorderIndex + 1, inorderS, i - 1);
-     int left = i - inorderS;
-     res->right = buildSubtree(preorder, inorder, preorderIndex + left + 1, i + 1, inorderE);
-     return res;
+         if (preorderIndex >= preorder.size() || inorderS > inorderE ){
+             return NULL;
+         }
+         int root = preorder[preorderIndex];
+         int i = 0;
+         for (i = 0; i < inorder.size(); ++i) { if (inorder[i] == root) { break; } }
+         TreeNode* res = new TreeNode(root);
+         res->left = buildSubtree(preorder, inorder, preorderIndex + 1, inorderS, i - 1);
+         int left = i - inorderS;
+         res->right = buildSubtree(preorder, inorder, preorderIndex + left + 1, i + 1, inorderE);
+         return res;
  }
 
  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
