9a1f459bb3e8a6e89d280298ded5fc590f926d60
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-02-26 21:50:08 -0800

    Update FindDuplicateFileInSystem.cpp

--- String/FindDuplicateFileInSystem.cpp
+++ String/FindDuplicateFileInSystem.cpp
@@ -1,36 +1,38 @@
 //https://leetcode.com/problems/find-duplicate-file-in-system/submissions/
+
+	/*
+		1. Create HT: <file_content> -> <vector of files with that content>
+		2. Iterate through HT. For each index, return all list of files with len > 1
+	*/
 class Solution {
 public:
+	vector<vector<string>> findDuplicate(vector<string>& paths) {
 
-    /*
-		  1. Create HT: <file_content> -> <vector of files with that content>
-		  2. Iterate through HT. For each index, return all list of files with len > 1
-	  */
-    vector<vector<string>> findDuplicate(vector<string>& paths) {
-        vector<vector<string>> res;
-        unordered_map<string, vector<string>> ht;
-        for (auto path : paths) {
-            stringstream ss(path);
-            string dir;
-            string file;
-            string fileContents;
-            getline(ss, dir, ' ');
-            dir += '/'; 
-            while (getline(ss, file, '(')) {
-                if (file[0] == ' ') {
-                    file = file.substr(1, file.size() - 1);
-                }
-                getline(ss, fileContents, ')');
-                string pathPlusFile = dir + file;
-                ht[fileContents].push_back(pathPlusFile); file = "";
-            }
-        }
+		vector<vector<string>> res;
+		unordered_map<string, vector<string>> ht;
+		for (auto path : paths) {
+			//"root/a 1.txt(abcd) 2.txt(efgh)"
+			stringstream ss(path);
+			string dir;
+			string file;
+			string fileContents;
+			getline(ss, dir, ' ');
+			dir += '/';
+			while (getline(ss, file, '(')) {
+				if (file[0] == ' ') {
+					file = file.substr(1, file.size() - 1);
+				}
+				getline(ss, fileContents, ')');
+				string pathPlusFile = dir + file;
+				ht[fileContents].push_back(pathPlusFile); file = "";
+			}
+		}
 
-        for (auto it = ht.begin(); it != ht.end(); ++it) {
-            if (it->second.size() > 1) {
-                res.push_back(it->second);
-            }
-        }
-		    return res;
-	  }
+		for (auto it = ht.begin(); it != ht.end(); ++it) {
+			if (it->second.size() > 1) {
+				res.push_back(it->second);
+			}
+		}
+		return res;
+	}
 };
