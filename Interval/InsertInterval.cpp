8cb69943a75f7a8d6babfa42d2769e18af2c30d1
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-03-01 19:56:56 -0800

    Update InsertInterval.cpp

--- Interval/InsertInterval.cpp
+++ Interval/InsertInterval.cpp
@@ -1,33 +1,51 @@
 //https://leetcode.com/problems/insert-interval/submissions/
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+
 class Solution {
 public:
-   vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
-    vector<Interval> left, right;
-    for (int i = 0; i < intervals.size(); i++) {
-        if (intervals[i].end < newInterval.start) {
-            left.push_back(intervals[i]);
+     
+vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int> newInterval) {
+    /*
+            intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+            left = [1, 2]
+            right = [12, 16]
+            mergedInterval = [min(interval[0], newInterval[0]),
+                              max(interval[1], newInterval[1])]
+    */
+    vector<vector<int>> res;
+    if (intervals.size() == 0) {
+        res.push_back(newInterval); return res;
+    }
+    vector<vector<int>> left; vector<vector<int>> right;
+    for (int i = 0; i < intervals.size(); ++i) {
+        vector<int> cur = intervals[i];
+        if (cur[1] < newInterval[0]) {
+            left.push_back(cur);
+            res.push_back(cur);
         }
-        if (intervals[i].start > newInterval.end) {
-            right.push_back(intervals[i]);
+        else if (cur[0] > newInterval[1]) {
+            right.push_back(cur);
         }
     }
-    int s = newInterval.start, e = newInterval.end;
-    if ((left.size()+right.size()) != intervals.size()) {
-        s = min(s, intervals[left.size()].start);
-        e = max(e, intervals[intervals.size()-right.size()-1].end);
+    int s; int e;
+    if (left.size() + right.size() == intervals.size()) {
+        res.push_back(newInterval);
+        for (int i = 0; i < right.size(); ++i) {
+            res.push_back(right[i]);
+        }
+        return res;
     }
-    Interval middle(s,e);
-    left.push_back(middle);
-    left.insert(left.end(), right.begin(), right.end());
-    return left;
+    vector<int> afterLeft = intervals[left.size()];
+    vector<int> beforeRight = intervals[intervals.size() - right.size()-1];
+    s = min(afterLeft[0], newInterval[0]);
+    e = max(beforeRight[1], newInterval[1]);
+    vector<int> mergedInterval = { s, e };
+    res.push_back(mergedInterval);
+    for (int i = 0; i < right.size(); ++i) {
+        res.push_back(right[i]);
+    }
+    
+    return res;
 }
+    
+    
 };
