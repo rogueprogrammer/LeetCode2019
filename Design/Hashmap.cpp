ed8454373d48a919fae209babcfd85508be78d9a
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-07-21 12:23:04 -0700

    Create Hashmap.cpp

---
+++ Design/Hashmap.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/design-hashmap/
+class MyHashMap {
+public:
+    int n = 1000000;
+    vector<int> data;
+
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        vector<int> v(n, -1);
+        data = v;
+    }
+
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        data[key] = value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return data[key];
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        data[key] = -1;
+    }
+};
+
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
