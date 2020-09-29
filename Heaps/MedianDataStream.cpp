447034b9b619a22f04660d1c10a842a52a22644b
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-09-28 21:20:17 -0700

    Update MedianDataStream.cpp

--- Heaps/MedianDataStream.cpp
+++ Heaps/MedianDataStream.cpp
@@ -1,28 +1,62 @@
-#include "Header.h"
-
-//https://leetcode.com/problems/find-median-from-data-stream/
+// https://leetcode.com/problems/find-median-from-data-stream/submissions/
+// runtime: O(logn) for rebalancing step in addnum(), and O(1) for get median
+// space complexity: O(n)
 
 class MedianFinder {
 public:
-    /** initialize your data structure here. */
+    /** 
+    The trick in this program is to visualize a sorted array organized like this:
+    [ low elements ]  [Median] [ high elements ]
+    
+    We need access to the high end of the low elements and the low end of the high elements
+    high end of low elements -> make a max heap of the low elements
+    low end of the high elements -> make a min heap of the high elements.
+    Now draw out the min/max heaps on an example to come up with the algo:
+    
+    [2, 7, 5, 4, 1]
+
+    maxHeap:
+    4
+    2
+    1
+
+
+    minHeap:
+
+    5
+    7
+    Anything greater than maxHeap's root belongs in minHeap, since minHeap contains elements that are in upper half.
+    Anything less than maxHeap's root belongs in maxHeap.
+    After insert operation, do rebalancing based on the Invariant: Size of the two heaps must not differ more than 1. 
+    For median, if both heaps have same amount of elments, return the average of the roots. else retrun the root of the heap that has more elements.
+
+
+    */
+    priority_queue<int, vector<int>> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
     MedianFinder() {
+
     }
 
     void addNum(int num) {
-        if (maxHeap.empty() || num > maxHeap.top()) {
+        if (maxHeap.empty()) {
+            maxHeap.push(num);
+        }
+        else if (num > maxHeap.top()) {
             minHeap.push(num);
         }
         else {
             maxHeap.push(num);
         }
-        
-        //resize if difference in sizes is 2 or more
-        if (!maxHeap.empty() && abs((int)maxHeap.size() - (int)minHeap.size()) >= 2) {
+        int minHeapSize = minHeap.size();
+        int maxHeapSize = maxHeap.size();
+        // rebalancing
+        if (maxHeapSize-minHeapSize > 1) {
             int top = maxHeap.top();
             maxHeap.pop();
             minHeap.push(top);
         }
-        else if (!minHeap.empty() && abs((int)minHeap.size() - (int)maxHeap.size()) >= 2) {
+        else if (minHeapSize-maxHeapSize > 1) {
             int top = minHeap.top();
             minHeap.pop();
             maxHeap.push(top);
@@ -30,40 +64,24 @@ public:
     }
 
     double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
+        if (minHeap.size() + maxHeap.size() == 0) {
+            return 0;
+        }
+        else if (minHeap.size() == maxHeap.size()) {
             return (maxHeap.top() + minHeap.top()) / 2.0;
         }
-        if (maxHeap.size() > minHeap.size()) {
+        else if (minHeap.size() > maxHeap.size()) {
+            return minHeap.top();
+        }
+        else {
             return maxHeap.top();
         }
-        return minHeap.top();
     }
-
-    priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
 };
 
-
-void main() {
-    /*MedianFinder* mf = new MedianFinder();
-    mf->addNum(1);
-    mf->addNum(2);
-    double median = mf->findMedian();
-    mf->addNum(3);
-    median = mf->findMedian();*/
-
-    MedianFinder* mf = new MedianFinder();
-    mf->addNum(-1);
-    double median = mf->findMedian();
-    mf->addNum(-2);
-    median = mf->findMedian();
-    mf->addNum(-3);
-    median = mf->findMedian();
-    mf->addNum(-4);
-    median = mf->findMedian();
-    mf->addNum(-5);
-    median = mf->findMedian();
-
-
-	system("PAUSE");
-}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
