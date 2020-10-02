12e50bdd604b680ee2d03a784a7dab6002370ef4
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-10-01 20:08:04 -0700

    Update MergeOneSortedArrayIntoAnotherSortedArray.cpp

--- TwoPointers/MergeOneSortedArrayIntoAnotherSortedArray.cpp
+++ TwoPointers/MergeOneSortedArrayIntoAnotherSortedArray.cpp
@@ -16,12 +16,9 @@ arr1 = [1 3 5]
 
 arr2 = [2 4 6 0 0 0]
 
-
-
 Output: arr2 = [1 2 3 4 5 6]
  */
-void merger_first_into_second(vector<int>& arr1, 
-vector<int>& arr2) {
+void merger_first_into_second(vector<int>& arr1, vector<int>& arr2) {
     
     /*
     Start iterating pointer at end of second array, which has the first available spot, and keep taking the max of the
