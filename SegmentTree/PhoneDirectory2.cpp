461ace8ab32bf85090c9970b5ac24f665fcd0823
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-10-17 21:39:08 -0700

    Update PhoneDirectory2.cpp

--- SegmentTree/PhoneDirectory2.cpp
+++ SegmentTree/PhoneDirectory2.cpp
@@ -1,18 +1,36 @@
-#include "Header.h"
-#include <time.h>
-#include <mutex>
-#include <thread>
-
+// https://leetcode.com/problems/design-phone-directory/
+/*
+this problem is to allocate an available number from (0-n)
+the easy solution is to maintain a set of numbers that are allocated and deallocated, and whenever a call to get() made, 
+return the first element from the deallocated set. if deallocated is empty, then use a variable which contains the next highest number that isn't allocated.
+this algo is O(1) runtime, and O(n) space.
+to optimize space complexity, can use a bitset where bitset[i] = 1 if a particular bit is available and 0 if already allocated.
+searching for next available bit can be utilized with segment tree search O(logn) algorithm if segment tree is used.
+left part of bitset is the segment tree
+bitset structure:
+{[0, ..., maxVal][maxVal+1, ..., 2maxVal]}
+0, ..., maxVal = segment tree
+maxVal+1, ..., 2maxVal = actual bitset of values that are 0, 1 if num at index i is allocated
+eg:
+consider bitset: 0, 1, 1, 1
+segment tree constructed by doing bitwise or on all the leaves:
+        1
+   0         1
+0    1     1    1
+thus, the bitset structure would be:
+[1, 0, 1, 0, 1, 1, 1]
+note that going from left tree nodes to actual bits, from parent to child => left child = (2*i)+1, right child = (2*i+2)
+going to parent is just: i-1/2
+segment tree search can be used to get to parents and children by checking if each child is 1 or not. if it's 1, that means that somewhere down the path, an available
+bit will be there.
+*/
 
 class PhoneDirectory {
 public:
     bitset<20000> bits; //let bits[i] = 1 if available 0 if allocated
     int maxVal;
     int boundary;
-    
-
-    /** Initialize your data structure here
-        @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+   
     PhoneDirectory(int maxNumbers) {
         bits.set(); //set all bits to 1, as they are all available
         maxVal = maxNumbers;
@@ -64,17 +82,14 @@ public:
 
     /** Check if a number is available or not. */
     bool check(int number) {
-        return number >= 0 and number < maxVal and bits[number + maxVal];
+        return number >= 0 && number < maxVal && bits[number + maxVal];
 
     }
 
     /** Recycle or release a number. */
     void release(int number) {
-        int i = maxVal + number;
-        while (i > 0) {
-            bits[i] = 1;
-            i /= 2;
-        }
+        bits[maxVal+number] = 1;
+        updateTree(maxVal+number);
     }
 
     void printBits() {
@@ -92,24 +107,15 @@ public:
 
 
 void main() {
-    /*
-
-
-    n=5
-
-
-    1       1        1         1
-
-
-    */
-    PhoneDirectory* ph = new PhoneDirectory(4);
-    cout <<"New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
+    
+	    PhoneDirectory* ph = new PhoneDirectory(5);
+	    cout <<"New ID: " << ph->get() << endl;
+	    cout << "New ID: " << ph->get() << endl;
+	    cout << "New ID: " << ph->get() << endl;
+	    cout << "New ID: " << ph->get() << endl;
+	    cout << "New ID: " << ph->get() << endl;
+	    cout << "New ID: " << ph->get() << endl;
+	    cout << "New ID: " << ph->get() << endl;
 
 
 	system("PAUSE");
