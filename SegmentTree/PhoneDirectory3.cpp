c6638a4c83b9d5120c26fb34ebe3e7f2383cfcfa
rogueprogrammer <dkhanna@uwaterloo.ca>
2020-10-21 20:56:31 -0700

    Update PhoneDirectory3.cpp

--- SegmentTree/PhoneDirectory3.cpp
+++ SegmentTree/PhoneDirectory3.cpp
@@ -1,126 +1,139 @@
-#include "Header.h"
-#include <time.h>
-#include <mutex>
-#include <thread>
 
+/*
+this problem is to allocate an available number from (0-n)
+the easy solution is to maintain a set of numbers that are allocated and deallocated, and whenever a call to get() made,
+return the first element from the deallocated set. if deallocated is empty, then use a variable which contains the next highest number that isn't allocated.
+this algo is O(1) runtime, and O(n) space.
+to optimize space complexity, can use a bitset where bitset[i] = 1 if a particular bit is available and 0 if already allocated.
+searching for next available bit can be utilized with segment tree search O(logn) algorithm if segment tree is used.
+left part of bitset is the segment tree
+bitset structure:
+{[0, ..., maxVal][maxVal+1, ..., 2maxVal]}
+0, ..., maxVal = segment tree
+maxVal+1, ..., 2maxVal = actual bitset of values that are 0, 1 if num at index i is allocated
+eg:
+consider bitset: 0, 1, 1, 1
+segment tree constructed by doing bitwise or on all the leaves:
+        1
+   0         1
+0    1     1    1
+thus, the bitset structure would be:
+[1, 0, 1, 0, 1, 1, 1]
+note that going from left tree nodes to actual bits, from parent to child => left child = (2*i)+1, right child = (2*i+2)
+going to parent is just: i-1/2
+segment tree search can be used to get to parents and children by checking if each child is 1 or not. if it's 1, that means that somewhere down the path, an available
+bit will be there.
+*/
 
 class PhoneDirectory {
 public:
-       
-    /*
-    allocate(4)  //{0, 1, 2, 3}
-    int x = get(); //x is 0
-    release(0);
-    //x = get();
-          1, 0
-       0, 1            1, 2
-     0, 3  0, 4     1, 5   1, 6
-    [0, 1, 2, 3, ][0, 1, 2, 3]
-
-    */
+    bitset<20000> bits; //let bits[i] = 1 if available 0 if allocated
     int maxVal;
-    int totalBits; // last bit in the bitset
-    bitset<20000> bits;
-    int offset;
-    PhoneDirectory(int maxNumbers) {
-        bits.set(); // set all bits to 1
-        maxVal = maxNumbers;
-        totalBits = maxVal - 1 + maxVal;
+    int boundary;
+    int numLeaves;
 
+    bool powerOf2(int x) {
+        return x & (x - 1) == 0;
+    }
+
+    PhoneDirectory(int maxNumbers) {
+        bits.set(); //set all bits to 1, as they are all available
+        maxVal = maxNumbers;
+        boundary = maxVal + maxVal - 1;
         // calculate offset
         if (!powerOf2(maxVal)) {
             int next = log2(maxVal) + 1;
-            offset = pow(2, next);
+            numLeaves = pow(2, next);
         }
         else {
-            offset = 0;
+            numLeaves = maxVal;
         }
     }
-    bool powerOf2(int x) {
-        return x & (x-1) == 0;
-    }
 
-    
+    /*
+    left part of bitset is the segment tree
+    bitset structure:
+    {[0, ..., maxVal][maxVal+1, ..., 2maxVal]}
+    0, ..., maxVal = segment tree
+    maxVal+1, ..., 2maxVal = actual bitset of values that are 0, 1 if num at index i is allocated
+    eg:
+    consider bitset: 0, 1, 1, 1
+    segment tree constructed by doing bitwise or on all the leaves:
+            1
+       0         1
+    0    1     1    1
+    thus, the bitset structure would be:
+    [1, 0, 1, 0, 1, 1, 1]    
+    */
+
+
     int get() {
-        int index = 0;
-        // the leaf nodes's index is among [offset, offset + MAX_ID]
+
+        int i = 0;
         while (1) {
-            int leftChild = index * 2 + 1;
-            int rightChild = index * 2 + 2;
-            if (rightChild >= offset + maxVal) {
+            int leftChild = i * 2 + 1;
+            int rightChild = i * 2 + 2;
+
+            // if left child out of range, break
+            if (leftChild >= (numLeaves + numLeaves - 1)) {
                 break;
-            }
+            } //if i is more than numLeaves - 1 + maxVal
+
             if (bits[leftChild] == 1) {
-                index = leftChild;
+                i = leftChild;
             }
-            else if (bits[index * 2 + 2] == 1) {
-                index = rightChild;
+            else if (bits[rightChild] == 1) {
+                i = rightChild;
             }
             else {
                 return -1;
             }
         }
-        // not found if the leaf nodes's index is outside [offset, offset + MAX_ID]
-        if (index >= offset + maxVal - 1) {
+        if (i >= numLeaves - 1 + maxVal) {
             return -1;
         }
-        bits[index] = 0;
-        updateTree(index);
-        // the id = index - offset + 1
-        return index - offset + 1;
 
+        bits[i] = 0;
+        updateTree(i);
+
+        return i - numLeaves + 1;
     }
 
     void updateTree(int i) {
         while (i > 0) {
             int parent = (i - 1) / 2;
             int sibling;
-            if (i % 2 == 1) { // i is left child
-                sibling = bits[i + 1]; //get right sibling
+            if (i % 2 == 1) { //left child
+                sibling = bits[i + 1];
             }
             else {
-                sibling = bits[i - 1]; // get left sibling
+                sibling = bits[i - 1];
             }
             bits[parent] = bits[i] | sibling;
             i = parent;
         }
     }
-    
+
     /** Check if a number is available or not. */
     bool check(int number) {
-        return bits[maxVal - 1 + number];
+        return bits[numLeaves - 1 + number];
+
     }
 
     /** Recycle or release a number. */
-    void release(int number) { 
-        bits[maxVal - 1 + number] = 1;
-        //update parents in segment tree
-        updateTree(number);
+    void release(int number) {
+        bits[numLeaves-1+number] = 1;
+        updateTree(numLeaves-1+number);
+    }
+
+    void printBits() {
+        for (int i = 0; i < maxVal * 2; ++i) {
+            cout << i << "  ";
+        }
+        cout << endl;
+        for (int i = 0; i < maxVal * 2; ++i) {
+            cout << bits[i] << "  ";
+        }
+        cout << endl;
     }
 };
-
-
-
-void main() {
-    /*
-
-
-    n=5
-
-
-    1       1        1         1
-
-
-    */
-    PhoneDirectory* ph = new PhoneDirectory(5);
-    cout <<"New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-    cout << "New ID: " << ph->get() << endl;
-
-
-	system("PAUSE");
-}
