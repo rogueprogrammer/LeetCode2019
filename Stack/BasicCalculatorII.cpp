ca5e8f60718b90b6aeb912680f177df81ef9ee2f
rogueprogrammer <dkhanna@uwaterloo.ca>
2023-05-25 19:46:46 -0700

    Update BasicCalculatorII.cpp

--- Stack/BasicCalculatorII.cpp
+++ Stack/BasicCalculatorII.cpp
@@ -2,52 +2,78 @@
 
 class Solution {
 public:
-    
-    
-    int calculate(string s) {
-        int res = 0;
-        /*
-        30+5/2
-        stack: 30, 2
-        Parse string, and each time you see *, /, then you push the result on the stack.
-        If you see subtraction sign, then make the next integer negative and push it on stack.
-        Add all integers on the stack.
-        */
-        stack<int> stk;
-        int i = 0;
-        string strNum = "";
-        while(i < s.size()){
-            if(isdigit(c)){
-                strNum += c;
-            } else if(c == ' '){ 
-               continue;   
-            }
-            else {
-                    int curNum = stoi(strNum);
-                    stk.push(curNum);
-                    strNum = "";
-                
-                    int nextNum;
-                    while(i < s.size() && s[i] == ' '){
-                        i++;
-                    }
-                    nextNum = s[i] - '0';
-                    if(c == '+'){
-                       continue;
-                    } else if(c == '-'){
-                        stk.push(nextNum * -1);
-                    } else if(c == '*'){
-                        int top = stk.top();
-                        stk.pop();
-                        stk.push(top * nextNum);
-                    } else if(c == '/'){
-                        int top = stk.top();
-                        stk.pop();
-                        stk.push(top / nextNum);
-                    }
-                }
+   int calculate(string s) {
+    /*
+    "3+2*2
+    1. Create a vector of strings: "3", +, 2, *, 2
+    2. Iterate through the strings:
+        -If it's a num, push it on the stack
+        -If its a "+", push next num onto the stack
+        -If its a "-", push next num *-1 onto the stack
+        -If its a "*", multiply top element with next num and pop stack, and push result on stack
+        -If its a "/", divide top elem by the next num, pop stack, and push result on stack
+
+    3. Iterate through stack, adding the elements together
+    */
+
+    //step 1
+    vector<string> strings;
+    string strNum = "";
+    for (int i = 0; i < s.size(); ++i) {
+        if (isdigit(s[i])) {
+            strNum += s[i];
         }
-        
-        return res;
+        else {
+            if (s[i] == ' ') {
+                continue;
+            }
+            strings.push_back(strNum);
+            strNum = "";
+            string strOp = ""; strOp += s[i];
+            strings.push_back(strOp);
+        }
     }
+    if (strNum != "") {
+        strings.push_back(strNum);
+    }
+
+    //step 2
+    stack<int> stk;
+    for (int i = 0; i < strings.size();) {
+        string cur = strings[i];
+        if (cur == "+") {
+            stk.push(stoi(strings[i + 1]));
+            i += 2;
+        }
+        else if (cur == "-") {
+            stk.push(stoi(strings[i + 1]) * -1);
+            i += 2;
+        }
+        else if (cur == "*") {
+            int top = stk.top();
+            stk.pop();
+            stk.push(top * stoi(strings[i + 1]));
+            i += 2;
+        }
+        else if (cur == "/") {
+            int top = stk.top();
+            stk.pop();
+            stk.push(top / stoi(strings[i + 1]));
+            i += 2;
+        }
+        else { //its a number
+            stk.push(stoi(strings[i]));
+            ++i;
+        }
+    }
+
+    //step 3
+    int res = 0;
+    while (!stk.empty()) {
+        int top = stk.top();
+        res += top;
+        stk.pop();
+    }
+    return res;
+}
 };
